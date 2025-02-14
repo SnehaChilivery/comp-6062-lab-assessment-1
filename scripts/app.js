9976a4a51181cca105e56a3493810aa6d44abdc3
SnehaChilivery <chsneha0203@gmail.com>
2025-02-13 19:41:54 -0500

    comp-6062-lab-assessment

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+console.log();
+const studentFullName = 'Sneha Chilivery';
+const studentNumber = '1201817';
+const result = 'studentFullName - studentNumber';
+console.log(result);
+const headerContent = document.querySelector("h1");
+
+headerContent.innerHTML= "Sneha Chilivery - 1201817";
+headerContent.classList.add("headingPrimary");
